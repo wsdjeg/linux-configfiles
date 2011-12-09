69a22dd9bd28c77b0762f387f7d9db1915ed9bda
Tom Wieland <tom.wieland@gmail.com>
2011-12-09 19:30:46 +0100

    Where are we going here?

--- .vim/init.lua
+++ .vim/init.lua
@@ -14,202 +14,192 @@
 
 package.path = '/home/tom/.vim/?.lua;' .. package.path
 package.path = '/home/tom/.vim/?/init.lua;' .. package.path
+package.path = '/home/tom/.vim/lib/?.lua;' .. package.path
+package.path = '/home/tom/.vim/lib/?/init.lua;' .. package.path
 package.path = './?/init.lua;' .. package.path
 
--- plumbing
-local commands = {}
--- queue a command
-function c(x)
-  table.insert(commands, x)
-end
+local inspect = require('inspector').inspect
 
-function runCommands()
-  for i, command in ipairs(commands) do
-    vim.command(command)
-  end
-end
+-- TODO: add LibStub
+local config = require('industrial.config').config
 
--- functions
+config(function()
+  -- settings
+  -- functionality
+  -- run in nocompatible, giving us more options. This should be the first command
+  set('compatible', false)
 
--- TODO: fix
--- c 'map <f11> :lua switchFont()<cr>'
+  -- load plugins
+  call('pathogen#runtime_append_all_bundles()')
+  call('pathogen#helptags()')
 
--- settings
--- functionality
--- run in nocompatible, giving us more options. This should be the first command
-c 'set nocompatible'
+  -- load filetype plugins, indentation and turn syntax highlighting on
+  cmd 'filetype plugin indent on'
+  cmd 'syntax on'
 
--- load plugins
-c 'call pathogen#runtime_append_all_bundles()'
-c 'call pathogen#helptags()'
+  -- set up file type detection
+  autocmd('BufEnter *.coffee set ft=coffee')
+  autocmd('BufEnter *.json   set ft=javascript')
 
--- load filetype plugins, indentation and turn syntax highlighting on
-c 'filetype plugin indent on'
-c 'syntax on'
+  -- make movement keys wrap to the next/previous line
+  set('whichwrap', 'b,s,h,l,<,>,[,]')
 
--- set up file type detection
-c 'autocmd BufEnter *.coffee set ft=coffee'
-c 'autocmd BufEnter *.json   set ft=javascript'
+  -- don't wrap lines
+  set('wrap', false)
 
--- make movement keys wrap to the next/previous line
-c 'set whichwrap=b,s,h,l,<,>,[,]'
+  -- make it possible to have buffers in the background
+  set('hidden', true)
 
--- don't wrap lines
-c 'set nowrap'
+  -- indentation
+  set('autoindent', true)
+  set('tabstop', 2)
+  set('shiftwidth', 2)
+  set('expandtab', true)
 
--- make it possible to have buffers in the background
-c 'set hidden'
-
--- indentation
-c 'set autoindent'
-c 'set tabstop=2'
-c 'set shiftwidth=2'
-c 'set expandtab'
-
--- backups
-c 'set writebackup'
-c 'set backup'
-c 'set swapfile'
-c 'set backupcopy=auto'
-c 'set backupdir=~/.vim/backup'
-c 'set directory=~/.vim/temp'
+  -- backups
+  set('writebackup', true)
+  set('backup', true)
+  set('swapfile', true)
+  set('backupcopy', 'auto')
+  set('backupdir', '~/.vim/backup')
+  set('directory', '~/.vim/temp')
 
 
--- display
--- show "invisible" characters
-c 'set list'
-c 'set listchars=tab:·\\ ,trail:+,extends:»,precedes:«'
+  -- display
+  -- show "invisible" characters
+  set('list', true)
+  set('listchars', 'tab:·\\ ,trail:+,extends:»,precedes:«')
 
--- don't show chars on split and fold lines
-c 'set fillchars=vert:\\ ,fold:\\ '
+  -- don't show chars on split and fold lines
+  set('fillchars', 'vert:\\ ,fold:\\ ')
 
--- turn on line numbers
-c 'set number'
+  -- turn on line numbers
+  set('number', true)
 
--- turn on a fold column of 1
-c 'set foldcolumn=1'
+  -- turn on a fold column of 1
+  set('foldcolumn', '1')
 
--- mapping settings
-c 'let mapleader=","'
+  -- mapping settings
+  let('mapleader', '","')
 
--- messages and info
-c 'set showcmd'
-c 'set showmode'
-c 'set noerrorbells'
-c 'set novisualbell'
-c 'set wildmenu'
-c 'set wildmode=list:longest'
-c 'set hlsearch'
-c 'set incsearch'
-
-
--- gui options
-c 'set guioptions=emg'
-
--- colorscheme
-c 'colorscheme solarized'
-c 'set background=light'
-
--- font
-require('fonts')
-
--- plugins
--- AutoComplPop
--- make it case sensitive
-c 'let g:acp_ignorecaseOption = 0'
-
--- minibufexpl
--- open at 1 buffer
-c 'let g:miniBufExplorerMoreThanOne = 1'
-
--- vimpager
-c 'let vimpager_use_gvim = 1'
-
--- syntastic
-c 'let g:syntastic_enable_signs = 1'
-c 'let g:syntastic_auto_loc_list = 1'
-
--- javascript indent
-c 'let g:SimpleJsIndenter_BriefMode = 1'
-
--- taglist
-c 'let Tlist_Ctags_Cmd="/usr/local/bin/ctags"'
-c 'let Tlist_Use_Right_Window = 1'
-c 'let Tlist_File_Fold_Auto_Close = 1'
-c 'let Tlist_Enable_Fold_Column = 0'
-c 'let Tlist_Show_One_File = 1'
-c 'let Tlist_Auto_Open = 0'
+  -- messages and info
+  set('showcmd', true)
+  set('showmode', true)
+  set('errorbells', false)
+  set('visualbell', false)
+  set('wildmenu', true)
+  set('wildmode', 'list:longest')
+  set('hlsearch', true)
+  set('incsearch', true)
 
 
--- maps
--- make the alt key behave as alt on osx
-if vim.eval('has("macunix")') == 1 then
-  c 'set macmeta'
-end
+  -- gui options
+  set('guioptions', 'emg')
 
--- normal mode maps
--- quick insert of newline
-c 'nmap <cr> o<esc>'
+  -- colorscheme
+  colorscheme('solarized')
+  set('background', 'light')
 
--- insert mode maps
--- remap escape to jj
-c 'inoremap jj <esc>'
+  -- font
+  --require('fonts')
 
--- all mode maps
--- ease of use / typos
-c 'map :Q :q'
-c 'map :W :w'
-c 'map :E :e'
+  -- plugins
+  -- AutoComplPop
+  -- make it case sensitive
+  let('g:acp_ignorecaseOption', '0')
 
--- OS register copy pasting
-c 'map <leader>d "+d'
-c 'map <leader>p "+p'
-c 'map <leader>y "+y'
+  -- minibufexpl
+  -- open at 1 buffer
+  let('g:miniBufExplorerMoreThanOne', '1')
 
--- mouse selection register copy pasting
-c 'map <leader>dd "*d'
-c 'map <leader>pp "*p'
-c 'map <leader>yy "*y'
+  -- vimpager
+  let('vimpager_use_gvim', '1')
 
--- switch buffers
-c 'map <tab> :tabnext<cr>'
-c 'map <S-tab> :tabprevious<cr>'
+  -- syntastic
+  let('g:syntastic_enable_signs', '1')
+  let('g:syntastic_auto_loc_list', '1')
 
-if vim.eval('has("macunix")') == 1 then
-  -- free up keymaps
-  c 'macmenu File.New\\ Tab key=<nop>'
-  c 'macmenu File.Close    key=<nop>'
+  -- javascript indent
+  let('g:SimpleJsIndenter_BriefMode', '1')
 
-  c 'map <D-t> :tabedit %<cr>'
-  c 'map <D-w> :SmartBw :tabclose<cr>'
-else
-  c 'map <S-t> :tabedit %<cr>'
-  c 'map <S-w> :bw %, tabclose<cr>'
-end
+  -- taglist
+  let('Tlist_Ctags_Cmd', '"/usr/local/bin/ctags"')
+  let('Tlist_Use_Right_Window', '1')
+  let('Tlist_File_Fold_Auto_Close', '1')
+  let('Tlist_Enable_Fold_Column', '0')
+  let('Tlist_Show_One_File', '1')
+  let('Tlist_Auto_Open', '0')
 
--- move through splits
-c 'map <C-h> <C-w>h'
-c 'map <C-j> <C-w>j'
-c 'map <C-k> <C-w>y'
-c 'map <C-l> <C-w>l'
 
--- open and reload .vimrc
-c 'map <leader>v :vsplit ~/.vimrc<cr>'
-c 'map <leader>V :bufdo :source ~/.vimrc<cr>:bufdo :filetype detect<cr>'
+  -- maps
+  -- make the alt key behave as alt on osx
+  platform('macunix', function()
+    set('macmeta')
+  end)
 
--- NERDTree
-c 'map <leader>[ :NERDTreeToggle<cr>'
-c 'map <leader>] :TlistToggle<cr>'
+  -- normal mode maps
+  -- quick insert of newline
+  nmap('<cr>', 'o<esc>')
 
--- resync syntax
-c 'map <leader>sy :syntax sync fromstart<cr>'
+  -- insert mode maps
+  -- remap escape to jj
+  inoremap('jj', '<esc>')
 
--- improved buffer delete
-c 'map <leader>d :SmartBd<cr>'
-c 'map <leader>c :SmartBw<cr>'
+  -- all mode maps
+  -- ease of use / typos
+  map(':Q', ':q')
+  map(':W', ':w')
+  map(':E', ':e')
 
--- Toggle background color
-c 'map <f12> :let &background = (&background == "dark" ? "light" : "dark")<cr>'
+  -- OS register copy pasting
+  map('<leader>d', '"+d')
+  map('<leader>p', '"+p')
+  map('<leader>y', '"+y')
 
-runCommands()
+  -- mouse selection register copy pasting
+  map('<leader>dd', '"*d')
+  map('<leader>pp', '"*p')
+  map('<leader>yy', '"*y')
 
+  -- switch buffers
+  map('<tab>', ':tabnext<cr>')
+  map('<S-tab>', ':tabprevious<cr>')
+
+  platform('macunix', function()
+    -- free up keymaps
+    cmd('macmenu File.New\\ Tab key=<nop>')
+    cmd('macmenu File.Close    key=<nop>')
+
+    map('<D-t>', ':tabedit %<cr>')
+    map('<D-w>', ':SmartBw :tabclose<cr>')
+  end)
+
+  platform('unix', function()
+    map('<S-t>', ':tabedit %<cr>')
+    map('<S-w>', ':bw %<cr>:tabclose<cr>')
+  end)
+
+  -- move through splits
+  map('<C-h>', '<C-w>h')
+  map('<C-j>', '<C-w>j')
+  map('<C-k>', '<C-w>y')
+  map('<C-l>', '<C-w>l')
+
+  -- open and reload .vimrc
+  map('<leader>v', ':vsplit ~/.vimrc<cr>')
+  map('<leader>V', ':bufdo :source ~/.vimrc<cr>:bufdo :filetype detect<cr>')
+
+  -- NERDTree
+  map('<leader>[', ':NERDTreeToggle<cr>')
+  map('<leader>]', ':TlistToggle<cr>')
+
+  -- resync syntax
+  map('<leader>sy', ':syntax sync fromstart<cr>')
+
+  -- improved buffer delete
+  map('<leader>d', ':SmartBd<cr>')
+  map('<leader>c', ':SmartBw<cr>')
+
+  -- Toggle background color
+  map('<f12>', ':let &background = (&background == "dark" ? "light" : "dark")<cr>')
+end)
