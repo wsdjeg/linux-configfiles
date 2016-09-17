91af7d2ddd96af029f3674dfa2a98f6f667034e5
Tom Wieland <tom.wieland@gmail.com>
2016-09-17 10:53:05 +0200

    Added neovim

--- .scripts/neovim.sh
+++ .scripts/neovim.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/env bash
 
-echo "NOT Installing neovim. TODO: Implement."
+echo "Installing neovim."
 
-#sudo apt-get install -y git > /dev/null
+sudo apt-get install -y \
+  software-properties-common \
+  xclip \
+  xsel \
+  python-dev \
+  python-pip \
+  python3-dev \
+  python3-pip \
+  > /dev/null
 
-mkdir -p ~/.config > /dev/null
+sudo add-apt-repository -y ppa:neovim-ppa/unstable > /dev/null
+sudo apt-get update > /dev/null
+sudo apt-get install -y neovim > /dev/null
+
+
+mkdir -p ~/.config
 if [ -d ~/.config/nvim ]; then
   rm ~/.config/nvim
 fi
 ln -s $REPO/.config/nvim ~/.config/nvim
+mkdir -p ~/.config/nvim/backup
+mkdir -p ~/.config/nvim/temp
